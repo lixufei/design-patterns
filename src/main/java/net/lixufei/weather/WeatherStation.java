facd15c1883e768d26af44c478a879889e93bee6
LiXuFei <xfli@thoughtworks.com>
2016-10-12 08:23:11 +0800

    create log files.

--- src/main/java/net/lixufei/weather/WeatherStation.java
+++ src/main/java/net/lixufei/weather/WeatherStation.java
@@ -7,14 +7,6 @@ public class WeatherStation {
     private static Logger logger = Logger.getLogger(WeatherStation.class);
 
     public static void main (String[] args) {
-//        BasicConfigurator.configure();
-
-//        WeatherData weatherData = new WeatherData();
-//        CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
-//        StaticsDisplay staticsDisplay = new StaticsDisplay(weatherData);
-//        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
-//        weatherData.setMeasures(23, 24, 100);
-//        weatherData.setMeasures(23, 24, 123.0f);
 
         logger.debug("This is debug message.");
         logger.info("This is info message.");
